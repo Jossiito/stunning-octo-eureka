a2d271d50cbf0fbd89b40d6c7a42120217ad8392
Jose <josemanuelalonsopozas@gmail.com>
2021-10-28 13:56:20 +0200

    Modificado archivo app.java y readme.md

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Jose Manuel Alonso Pozas");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("28/10/2021");
+    System.out.println("29/10/2021");
   }
   
 }
